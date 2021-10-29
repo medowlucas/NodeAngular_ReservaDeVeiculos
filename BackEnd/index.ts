5c90ef46199e4cad67da925313045f4e87d88d03
Lucas Lopes de Souza Guilha <medowlucas@gmail.com>
2021-10-29 00:11:24 -0300

    classe instanciada e rodando na api

--- BackEnd/index.ts
+++ BackEnd/index.ts
@@ -1,14 +1,13 @@
 import {Veiculo} from '../BackEnd/model/veiculo';
 
-var veiculo = new Veiculo();
+var veiculo = new Veiculo('ABC123');
 
 import express from 'express';
 const porta = 3000;
 const app = express(); 
 const PORT = 3000;
-app.get('/', (req, res) => res.send("aaaa"));
+app.get('/', (req, res) => res.send(veiculo.getPlaca()));
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
-    console.log(veiculo.getPlaca());
   });
 
