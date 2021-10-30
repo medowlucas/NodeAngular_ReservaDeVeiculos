98d4864b2d623e728880719082bd94a34ef7fb7b
Lucas Lopes de Souza Guilha <medowlucas@gmail.com>
2021-10-30 00:35:29 -0300

    modelada a classe veiculo com as propriedades e criado objetos de teste no index.js

--- BackEnd/index.ts
+++ BackEnd/index.ts
@@ -1,12 +1,19 @@
 import {Veiculo} from '../BackEnd/model/veiculo';
 
-var veiculo = new Veiculo('ABC123');
+var veiculo = new Veiculo('aaa','Mustang',1990,true); //instancia no construtor
+
+var veiculo2 = new Veiculo(); //instancia e seta depois 
+veiculo2.setModelo('Ferrari');
+veiculo2.setAno(2002);
+veiculo2.setPlaca('QQQ2013K');
 
 import express from 'express';
 const porta = 3000;
 const app = express(); 
 const PORT = 3000;
-app.get('/', (req, res) => res.send(veiculo.getPlaca()));
+app.get('/1', (req, res) => res.send(veiculo));
+app.get('/2', (req, res) => res.send(veiculo2));
+
 app.listen(PORT, () => {
     console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`);
   });
--- BackEnd/model/veiculo.ts
+++ BackEnd/model/veiculo.ts
@@ -1,11 +1,51 @@
 export class Veiculo{
-    private placa;
 
-    constructor(placa?:string){
-        this.placa = placa;    
+    private placa:string;
+    private modelo:string;
+    private ano:number;
+    private estado:boolean;
+
+    constructor(placa?:string,
+        modelo?:string,
+        ano?:number,
+        estado?:boolean
+    ){
+        this.placa = placa!;
+        this.modelo = modelo!;
+        this.ano = ano!;
+        this.estado = estado!;
     }
 
-    getPlaca(){
-        return this.placa;
+    public setPlaca(placa:string): void{
+        this.placa = placa;
     }
+
+    public getPlaca():string{
+        return this.placa != ''? this.placa:'Placa não definida';
+    }
+
+    public setModelo(modelo:string):void{
+        this.modelo = modelo;
+    }
+
+    public getModelo():string{
+        return this.modelo;
+    }
+
+    public setAno(ano:number):void{
+        this.ano = ano;
+    }
+
+    public getAno():number{
+        return this.ano;
+    }
+
+    public setEstado(estado:boolean):void{
+        this.estado = estado;
+    }
+
+    public getEstado():string{
+        return this.estado? 'Disponível' : 'Manutenção';
+    }
+    
 }
