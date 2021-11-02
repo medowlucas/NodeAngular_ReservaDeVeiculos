37908af9e2a239c0b448948f9290ccb0d1af2f58
Lucas Lopes de Souza Guilha <medowlucas@gmail.com>
2021-11-02 15:32:54 -0300

    configurado o componente Cadastro de Veiculo

--- FrontEnd/web/src/app/app.module.ts
+++ FrontEnd/web/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { ListarService } from './veiculos/listarService';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { ReservaVeiculoComponent } from './veiculos/reserva-veiculo/reserva-veic
     BrowserModule,
     HttpClientModule,
     AppRoutingModule,
+    FormsModule
   ],
   providers: [ListarService],
   bootstrap: [AppComponent]
--- FrontEnd/web/src/app/veiculos/cadastro-veiculo/cadastro-veiculo.component.ts
+++ FrontEnd/web/src/app/veiculos/cadastro-veiculo/cadastro-veiculo.component.ts
@@ -1,3 +1,5 @@
+import { Veiculo } from './../Veiculo';
+import { ListarService } from './../listarService';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -5,11 +7,43 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './cadastro-veiculo.component.html',
   styleUrls: ['./cadastro-veiculo.component.css']
 })
-export class CadastroVeiculoComponent implements OnInit {
 
-  constructor() { }
+export class CadastroVeiculoComponent{
 
-  ngOnInit(): void {
+  public _rows:Array<Veiculo>=[];
+  public veiculo:any;
+  public placa:string = '';
+  public modelo!:string;
+  public ano:number = 0;
+  public estado:boolean = true;
+
+
+  constructor(private _listarService: ListarService) {
+  }
+ 
+  async insertVeiculo(veiculo:Veiculo){
+    await this._listarService.postVeiculo(veiculo);
+    window.location.reload()
+  }
+
+  onSubmit(){
+    this.veiculo = new Veiculo(this.placa,this.modelo,Number(this.ano),this.estado);
+  }
+
+  formClean(){
+    this.placa = '';
+    this.modelo = '';
+    this.ano = 0;
+    this.estado = true;
+    window.location.reload();
+  }
+
+  confirmacao() {
+    if(confirm(`Confirma o Cadastrado desse Veículo?`)) {
+      this.insertVeiculo(this.veiculo);
+      alert(`Veículo Cadastrado com Sucesso!`);
+    }
+    this.formClean();
   }
 
 }
